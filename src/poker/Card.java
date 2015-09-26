02ed2f40b6e3440d37d4c84c6fb1d66e49c99d61
alexcameron02 <onboardrope@gmail.com>
2015-09-26 03:17:36 -0400

    Added Card class. Updated CardObjectTest.java. Updated ReadMe

---
+++ src/poker/Card.java
@@ -0,0 +1,18 @@
+package poker;
+import java.util.*;
+
+public class Card {
+	
+	private int rank;
+	private String suit;
+	
+	//Constructor
+	public Card(String rankSuit){
+
+	}
+	
+	public int getRank(){
+		return -1;
+	}
+	
+}
--- src/test/CardObjectTest.java
+++ src/test/CardObjectTest.java
@@ -1,14 +1,24 @@
 package test;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
+import junit.framework.Assert;
+import poker.Card;
+
+@SuppressWarnings("deprecation")
 public class CardObjectTest {
 
 	@Test
 	public void create_card_and_return_rank(){
+		//Given
+		String rankSuit = "TwoClubs";
 		
-		fail("Theres no code anywhere!");		
+		//When
+		Card testCard = new Card(rankSuit);
+		
+		//Then
+		Assert.assertTrue("The rank of the card should be 2", testCard.getRank() == 2);
+				
 	}
 	
 }
