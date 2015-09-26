9e91416e6205b6563b4fd9edbbb7e94a7fee896b
alexcameron02 <onboardrope@gmail.com>
2015-09-26 01:34:58 -0400

    Initial commit, kinda

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Don't read me yet, im not finished!
+
+Given Requirement: Players can input their hands in the form...
+	Derived requirement: A card can be input into the system
+		Test methods: create_card_and_return_rank
---
+++ src/test/CardObjectTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CardObjectTest {
+
+	@Test
+	public void create_card_and_return_rank(){
+		
+		fail("Theres no code anywhere!");		
+	}
+	
+}
